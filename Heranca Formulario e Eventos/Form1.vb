81dad59f5dff887f96727a13d34a2b76db836b5b
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 19:05:49 -0300

    old project migrated

---
+++ Heranca Formulario e Eventos/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Inherits Form2
+
+    Public Sub New()
+        'MyBase.New("sttt")
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Sub b1_clicou(y As String) Handles Me.BClicou
+        MsgBox("b1 clicou" & y)
+    End Sub
+End Class
---
+++ Heranca Formulario e Eventos/Form2.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form2
+
+    Private s As String
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Public Sub New(s As String)
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.s = s
+    End Sub
+
+    'Public Delegate Sub B1Delegate()
+    Protected Event BClicou(s As String) 'As B1Delegate
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        RaiseEvent BClicou("xyz")
+    End Sub
+
+End Class
